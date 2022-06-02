722215c6f37769c59653a9af983fc9dfd78c90f7
Maxim <toyscrew1@gmail.com>
2022-06-02 23:38:52 +0300

    Подключен файл в index.html и исправлен нейминг и стилистика кода

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
+const MAX_LENGTH_COMMENT = 140;
+
+function isStringMaxLength(checkString, maxStringLength)
+{
+  return checkString.length <= maxStringLength;
+}
+
+isStringMaxLength('1560', MAX_LENGTH_COMMENT);
+
 //Функция взята с сайта https://developer.mozilla.org, за исключением условий
 function getRandomInt(min, max)
 {
-  if (min < 0)
+  if(min < 0)
   {
     return false;
   }
 
-  if (max <= min)
+  if(max <= min)
   {
     return false;
   }
@@ -14,11 +23,3 @@ function getRandomInt(min, max)
 }
 
 getRandomInt(1, 5);
-
-
-const MAX_COMMENT = 140;
-
-function maxLength(commentLength, maxCommentLength) {
-  return commentLength.length <= maxCommentLength;
-}
-maxLength('1560', MAX_COMMENT);
