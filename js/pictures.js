65eb295158cd892182e2bf2b61e35b52aa8045ef
Maxim <toyscrew1@gmail.com>
2022-06-25 09:15:21 +0300

    Исправлена ошибка в классе объекта ссылающемся на картинку

--- js/pictures.js
+++ js/pictures.js
@@ -9,7 +9,7 @@ const pictureGaleryFragment = document.createDocumentFragment();
 
 pictureGalery.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('picture__img').src = url;
+  pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments;
   pictureGaleryFragment.appendChild(pictureElement);
