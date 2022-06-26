c76613aed25814e95a0fb99f974231e27635137f
Maxim <toyscrew1@gmail.com>
2022-06-26 10:16:01 +0300

    Выполнено задание отрисуй меня полностью 2

--- js/big-picture.js
+++ js/big-picture.js
@@ -3,29 +3,44 @@ import {
   storageComments,
 } from './create-photo.js';
 
-const picture = document.querySelector('.picture');
+const pictures = document.querySelectorAll('.picture');
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
 const socialComments = document.querySelector('.social__comments');
-const pictureElement = getPhotoPost;
-const socialCommentsElement = storageComments;
-const bigPictureFragment = document.createDocumentFragment();
+const commentCount = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
 
-picture.addEventListener('click', () => {
-  bigPicture.classList.remove('hidden');
+pictures.forEach((picture) => {
+  picture.addEventListener('click', () => {
+    bigPicture.classList.remove('hidden');
+
+    getPhotoPost.forEach(({url, likes, comments, description}) => {
+      bigPicture.querySelector('.big-picture__img').src = url;
+      bigPicture.querySelector('.likes-count').textContent = likes;
+      bigPicture.querySelector('.comments-count').textContent = comments.length;
+      bigPicture.querySelector('.social__caption').textContent = description;
+    });
+
+    storageComments.forEach(({avatar, name, message}) => {
+      socialComments.querySelector('.social__picture').src = avatar;
+      socialComments.getAttribute.alt = name;
+      socialComments.querySelector('.social__text').textContent = message;
+    });
+
+    if (bigPicture !== '.hidden') {
+      commentCount.classList.add('hidden');
+      commentsLoader.classList.add('hidden');
+      document.body.classList.add('modal-open');
+
+      bigPictureCancel.addEventListener('click', () => {
+        bigPicture.classList.add('hidden');
+      });
+    }
+
+    document.addEventListener('keydown', (evt) => {
+      if (evt.keyCode === 27) {
+        bigPicture.classList.add('hidden');
+      }
+    });
+  });
 });
-
-pictureElement.forEach(({url, likes, comments}) => {
-  bigPicture.querySelector('.big-picture__img').src = url;
-  bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.comments-count').textContent = comments;
-  bigPictureFragment.appendChild(bigPicture);
-});
-
-socialCommentsElement.forEach(({avatar, name, message}) => {
-  socialComments.querySelector('.social__picture').src = avatar;
-  socialComments.alt = name;
-  socialComments.querySelector('.social__text').textContent = message;
-  bigPictureFragment.appendChild(socialComments);
-});
-
-
